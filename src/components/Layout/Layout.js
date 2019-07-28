cc47eae106607255e6bb8f39be0c8a4700588201
Andrei Egorov <andreyegorov1992@gmail.com>
2019-07-28 15:18:56 -0700

    add SideDrawer no mobilefirst yet

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
-
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer"
 const Layout = props => {
   return (
     <React.Fragment>
       <Toolbar />
+      <SideDrawer/>
       <main className={classes.Content}>{props.children}</main>
     </React.Fragment>
   );
